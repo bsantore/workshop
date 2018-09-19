d03f54d30ed5a42439c4a5a1b5c44d83ea20f588
Brett Santore <bsantore@kbra.com>
2018-09-18 20:01:48 -0400

    add default database credentials
    This should never been done in a real open source project

--- html/config.php
+++ html/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_NAME', '');
-define('DB_USER', '');
+define('DB_NAME', 'phpstorm');
+define('DB_USER', 'phpstorm');
 define('DB_PASSWORD', '');
-define('DB_HOST', '');
+define('DB_HOST', '127.0.0.1');
 
 define('DEFAULT_ADMIN_USERNAME', 'admin');
 define('DEFAULT_ADMIN_PASSWORD', 'admin');
