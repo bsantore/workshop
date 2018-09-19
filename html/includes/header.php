0a56d826c77e0d187c76a10275a029021197b38e
Brett Santore <bsantore@kbra.com>
2018-09-18 20:16:15 -0400

    use php phl favicon

--- html/includes/header.php
+++ html/includes/header.php
@@ -6,6 +6,8 @@
     <title><?php echo SITE_TITLE; ?></title>
     <link href="styles/site.css" rel="stylesheet" type="text/css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+
 </head>
 <body>
 <div class="outer-wrapper">
