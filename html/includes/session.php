f176f848229671c83bee45454b0d307bc536cce4
Brett Santore <bsantore@kbra.com>
2018-09-19 19:34:38 -0400

    fix session management so we aren't passing a reference into the bind param method

--- html/includes/session.php
+++ html/includes/session.php
@@ -45,9 +45,10 @@ function confirm_is_admin($session)
 function is_admin($session)
 {
     global $databaseConnection;
+    $userId= $session->get('userid');
     $query = "SELECT user_id FROM users_in_roles UIR INNER JOIN roles R on UIR.role_id = R.id WHERE R.name = 'admin' AND UIR.user_id = ? LIMIT 1";
     $statement = $databaseConnection->prepare($query);
-    $statement->bind_param('d', $session->get('userid'));
+    $statement->bind_param('d', $userId);
     $statement->execute();
     $statement->store_result();
     return $statement->num_rows == 1;
