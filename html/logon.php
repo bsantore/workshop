6a12f8c1dbed3eb66c06bd49224719d20d57a6d3
Brett Santore <bsantore@kbra.com>
2018-10-02 20:09:07 -0400

    extract log on page to template file

--- html/logon.php
+++ html/logon.php
@@ -33,32 +33,7 @@ if (isset($_POST['submit'])) {
         echo "Username/password combination is incorrect.";
     }
 }
-?>
-    <div id="main">
-        <h2>Log on</h2>
 
-        <form action="logon.php" method="post">
-            <fieldset>
-                <legend>Log on</legend>
-                <ol>
-                    <li>
-                        <label for="username">Username:</label>
-                        <input type="text" name="username" value="" id="username"/>
-                    </li>
-                    <li>
-                        <label for="password">Password:</label>
-                        <input type="password" name="password" value="" id="password"/>
-                    </li>
-                </ol>
-                <input type="submit" name="submit" value="Submit"/>
-
-                <p>
-                    <a href="index.php">Cancel</a>
-                </p>
-            </fieldset>
-        </form>
-    </div>
-
-<?php
+include '../templates/log-on.php';
 include SITE_PATH . '/includes/footer.php';
 include SITE_PATH . '/includes/closeDB.php';
