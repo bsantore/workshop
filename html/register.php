469d0d0b7833f0a2ec3a9513014a25f3262649c5
Brett Santore <bsantore@kbra.com>
2018-10-02 20:10:10 -0400

    extract register page to template file

--- html/register.php
+++ html/register.php
@@ -35,33 +35,7 @@ if (isset($_POST['submit'])) {
         echo "Failed registration";
     }
 }
-?>
-    <div id="main">
-        <h2>Register an account</h2>
-
-        <form action="register.php" method="post">
-            <fieldset>
-                <legend>Register an account</legend>
-                <ol>
-                    <li>
-                        <label for="username">Username:</label>
-                        <input type="text" name="username" value="" id="username"/>
-                    </li>
-                    <li>
-                        <label for="password">Password:</label>
-                        <input type="password" name="password" value="" id="password"/>
-                    </li>
-                </ol>
-                <input type="submit" name="submit" value="Submit"/>
-
-                <p>
-                    <a href="index.php">Cancel</a>
-                </p>
-            </fieldset>
-        </form>
-    </div>
-
-<?php
+include '../templates/register.php';
 include SITE_PATH . '/includes/footer.php';
 
 include SITE_PATH . '/includes/closeDB.php';
